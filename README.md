6f06bfd54df10fc183d491fddd68a7e875cf938b
miyaryj <miya.ryj@gmail.com>
2020-04-06 14:55:13 +0900

    Detect unused channels

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# slack-channel-monitor
+
+Detect unused channels and post alert message.
+
+## Slack App Configuration
+
 ### OAuth & Permissions
 
 - Add below **Bot Token Scopes** in **Scope**
@@ -15,3 +21,37 @@
 - Copy `OAuth Access Token` and use as `USER_OAUTH_TOKEN`
 
 - Copy `Bot User OAuth Access Token` and use as `BOT_OAUTH_TOKEN`
+
+## Usage
+
+### Installation
+
+```
+npm install
+```
+
+### Environment values
+
+- `USER_OAUTH_TOKEN` : App's OAuth Access Token. See above
+- `BOT_OAUTH_TOKEN` : App's Bot User OAuth Access Token. See above
+- `WHITELIST` : Channel names to ignore (comma-separated)
+
+Specify environment values in terminal
+```
+set USER_OAUTH_TOKEN=xxx BOT_OAUTH_TOKEN=xxx npm start
+```
+or in `.env` file
+```
+USER_OAUTH_TOKEN="xxx"
+BOT_OAUTH_TOKEN="xxx"
+WHITELIST="aaa,bbb"
+```
+
+### Execution
+
+Detect unused channels from all public channels and post alert message to the channel and admin channel
+
+```
+npm start
+```
+
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const ProxyAgent = require("proxy-agent");
 const moment = require("moment");
 
 const ENDPOINT = "https://slack.com/api";
+const ALERT_CHANNEL = "admin";
 
 const userToken = process.env.USER_OAUTH_TOKEN;
 const botToken = process.env.BOT_OAUTH_TOKEN;
+const whitelist = process.env.WHITELIST.split(",");
 
 const agent = new ProxyAgent();
 checkUnusedChannels();
@@ -16,16 +18,39 @@ async function checkUnusedChannels() {
   const channels = await getChannels();
   console.log(`Channels: ${channels.length}`);
 
+  const unused = [];
   for (const c of channels) {
     const messages = await getChannelMessages(c.id);
     const latest = messages.filter(m => !m.subtype || !m.subtype.startsWith("channel_"))[0];
-    if (!latest) {
-      console.log(`No latest: ${c.name}`);
-      continue;
+    if (latest) {
+      if (current.diff(moment.unix(latest.ts), "months") >= 12) {
+        console.log(`Unused: ${c.name} ([${latest.subtype}] ${latest.text.substr(0, 20)})`);
+        c.since = moment.unix(latest.ts).format("YYYY-MM-DD");
+      }
+    } else {
+      console.log(`No latest: ${c.name}; Created: ${moment.unix(c.created).format("YYYY-MM-DD")}`);
+      if (current.diff(moment.unix(c.created), "months") >= 12) {
+        c.since = moment.unix(c.created).format("YYYY-MM-DD");
+      }
     }
-    if (current.diff(moment.unix(latest.ts), "months") >= 12) {
-      console.log(`Unused: ${c.name} ([${latest.subtype}] ${latest.text.substr(0, 20)})`);
+    if (c.since && whitelist.indexOf(c.name) < 0) {
+      unused.push(c);
     }
+  }
+
+  if (unused.length) {
+    const summary = ["Unused channels found!\n"];
+    await Promise.all(unused.map(c => {
+      summary.push(`<#${c.id}> (since ${c.since})`);
+
+      const indivisual = [
+        `This channel will be archived because unsed since ${c.since}.`,
+        `Please contact admin if you want this channel kept as active.`
+      ];
+      return postMessage(c.id, indivisual.join("\n"));
+    }));
+    console.log(summary.join("\n"));
+    await postMessage(ALERT_CHANNEL, summary.join("\n"));
   }
 }
 
@@ -48,11 +73,20 @@ async function getChannelMessages(channelId) {
 }
 
 async function postMessage(channelId, text) {
-  const resp = await fetch(`${ENDPOINT}/chat.postMessage?token=${botToken}&channel=${channelId}&text=${text}`, {
+  var payload = {
+    channel: channelId,
+    text: text
+  };
+  const resp = await fetch(`${ENDPOINT}/chat.postMessage`, {
     agent,
-    method: "POST"
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: `Bearer ${botToken}`
+    },
+    body: JSON.stringify(payload)
   });
   const body = await resp.json();
-  if (!body.ok) console.error(`getChannelMessages failed: ${body.error}`);
+  if (!body.ok) console.error(`postMessage failed: ${body.error}`);
   return true;
 }
